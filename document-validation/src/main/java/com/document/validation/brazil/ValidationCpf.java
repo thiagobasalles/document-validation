ca2d95359afa249d1de3d45fb81c342e84e555ee
Thiago Bento de Almeida Salles <thiagobasalles@gmail.com>
2016-11-29 16:53:10 -0200

    Permitindo o cpf 00000000000, para fins de não obrigatoriedade.

--- document-validation/src/main/java/com/document/validation/brazil/ValidationCpf.java
+++ document-validation/src/main/java/com/document/validation/brazil/ValidationCpf.java
@@ -35,7 +35,7 @@ public class ValidationCpf {
 	}
 
 	private boolean verifySameNumber(String cpf) {
-		if (cpf.equals("00000000000") || cpf.equals("11111111111") || cpf.equals("22222222222")
+		if (cpf.equals("11111111111") || cpf.equals("22222222222")
 				|| cpf.equals("33333333333") || cpf.equals("44444444444") || cpf.equals("55555555555")
 				|| cpf.equals("66666666666") || cpf.equals("77777777777") || cpf.equals("88888888888")
 				|| cpf.equals("99999999999")) {
